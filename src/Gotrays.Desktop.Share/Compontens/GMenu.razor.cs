02a2dfaf43843866b1c86b31b81e5bfba2115c4e
token <239573049@qq.com>
2023-05-31 00:27:11 +0800

    优化布局样式

---
+++ src/Gotrays.Desktop.Share/Compontens/GMenu.razor.cs
@@ -0,0 +1,31 @@
+﻿using BlazorComponent;
+using Microsoft.JSInterop;
+
+namespace Gotrays.Desktop.Share.Compontens
+{
+    public partial class GMenu
+    {
+        private StringNumber _selectedItem = 0;
+        private Item[] _items = new Item[]
+        {
+           new Item { Text= "My Files", Icon= "mdi-folder" },
+           new Item { Text= "Shared with me", Icon= "mdi-account-multiple" },
+           new Item { Text= "Starred", Icon= "mdi-star" },
+           new Item { Text= "Recent", Icon= "mdi-history" },
+           new Item { Text= "Offline", Icon= "mdi-check-circle" },
+           new Item { Text= "Uploads", Icon= "mdi-upload" },
+           new Item { Text= "Backups", Icon= "mdi-cloud-upload" }
+        };
+
+        class Item
+        {
+            public string Text { get; set; }
+            public string Icon { get; set; }
+        }
+
+        private async Task OnGitHub()
+        {
+            await JSRuntime.InvokeVoidAsync("open", "https://github.com/239573049/gotrays-desktop");
+        }
+    }
+}
--- src/Gotrays.Desktop.Share/Shared/MainLayout.razor.cs
+++ src/Gotrays.Desktop.Share/Shared/MainLayout.razor.cs
@@ -3,4 +3,16 @@
 public partial class MainLayout
 {
     private bool _drawer;
+
+    private bool _home;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (NavigationManager.Uri == "http://localhost/")
+        {
+            _home = true;
+        }
+
+        await Task.CompletedTask;
+    }
 }
